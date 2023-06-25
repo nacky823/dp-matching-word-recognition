f315ffb4e593e2016856edeb9cf4e77bd513d353
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-06-25 18:19:57 +0900

    update calculator

--- core/calculator.cpp
+++ core/calculator.cpp
@@ -1,2 +1,3 @@
 // SPDX-FileCopyrightText: 2023 Yuki NAGAKI youjiyongmu4@gmail.com
 // SPDX-License-Identifier: GPL-2.0
+#include "calculator.hpp"
--- core/calculator.hpp
+++ core/calculator.hpp
@@ -1,2 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Yuki NAGAKI youjiyongmu4@gmail.com
 // SPDX-License-Identifier: GPL-2.0
+#ifndef CALCULATOR_HPP_
+#define CALCULATOR_HPP_
+
+#endif // CALCULATOR_HPP_
